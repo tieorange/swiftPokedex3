ad83a41653bb019b9e57e6dd224579219f40295e
Andrii Kovalchuk <tieorange@gmail.com>
2016-10-06 21:29:00 +0200

    cell

---
+++ pokedex3/PokeCell.swift
@@ -0,0 +1,13 @@
+//
+//  PokeCell.swift
+//  pokedex3
+//
+//  Created by Andrii Kovalchuk on 06/10/2016.
+//  Copyright © 2016 Andrii Kovalchuk. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+}
